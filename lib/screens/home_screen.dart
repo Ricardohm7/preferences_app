d066c11582653ebfe3a95a5a62dfecb35752286c
Ricardo Hernandez <ricardo.hernandez@andonix.com>
2023-02-11 19:06:51 -0600

    Create Side menu in progress

---
+++ lib/screens/home_screen.dart
@@ -0,0 +1,20 @@
+import 'package:flutter/material.dart';
+
+import '../widgets/widgets.dart';
+
+class HomeScreen extends StatelessWidget {
+  static const String routerName = 'Home';
+
+  const HomeScreen({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      drawer: const SideMenu(),
+      appBar: AppBar(title: const Text('Home')),
+      body: const Center(
+        child: Text('HomeScreen'),
+      ),
+    );
+  }
+}
